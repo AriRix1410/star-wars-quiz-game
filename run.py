4d009dc14d93e2c8d294f5675a649875d11ce5f9
AriRix1410 <aa.fraserhaste@gmail.com>
2022-04-05 20:50:06 +0000

    Add code to validate user input for question answers

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 from questions import question_prompts
 from question_class import Question
 
-name = input('Please enter your name ')
-print(f"May The Force Be With You {name}")
+print("""
+  ____ _____  _    ____   __        ___    ____  ____     ___  _   _ ___ _____
+ / ___|_   _|/ \  |  _ \  \ \      / / \  |  _ \/ ___|   / _ \| | | |_ _|__  /
+ \___ \ | | / _ \ | |_) |  \ \ /\ / / _ \ | |_) \___ \  | | | | | | || |  / / 
+  ___) || |/ ___ \|  _ <    \ V  V / ___ \|  _ < ___) | | |_| | |_| || | / /_ 
+ |____/ |_/_/   \_\_| \_\    \_/\_/_/   \_\_| \_\____/   \__\_\\___/|___/____|
+                                                                              
+""")
+
+name = input("Please enter your name\n")
+print(f"May The Force Be With You {name}\n")
+print("*********************************")
 
 
 questions = [
-    # Assigns correct answer to questions in list
+    # Assigns correct answer to questions
     Question(question_prompts[0], "b"),
     Question(question_prompts[1], "c"),
     Question(question_prompts[2], "a"),
@@ -18,17 +28,21 @@ questions = [
 
 def run_quiz(questions):
     """
-    Fuction loops through questions and checks answer given
+    Function loops through questions and checks answer given
     against correct answer. If correct answer given
     then score increases by 1.
     """
     score = 0
     for question in questions:
-        answer = input(question.prompt)
-        answer = answer.lower()
+        answer = input(question.prompt).lower()
+        while answer not in {'a', 'b', 'c'}:
+            answer = input("Invalid answer, try again").lower()
         if answer == question.answer:
             score += 1
-    print(f"{name} got {score} out of {str(len(questions))} questions correct")
+            print(f"Well done {name}. That is correct\n")
+        else:
+            print(f"Sorry {name}. That answer is incorrect\n")
+    print(f"{name} got {score} out of {len(questions)} questions correct")
 
 
 def play_again():
