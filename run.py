1af0d48c893de68e0485b645762372a9f181f4a3
AriRix1410 <aa.fraserhaste@gmail.com>
2022-04-05 18:03:55 +0000

    Add function to play game again at end of game

--- run.py
+++ run.py
@@ -17,12 +17,36 @@ questions = [
 
 
 def run_quiz(questions):
+    """
+    Fuction loops through questions and checks answer given
+    against correct answer. If correct answer given
+    then score increases by 1.
+    """
     score = 0
     for question in questions:
         answer = input(question.prompt)
+        answer = answer.lower()
         if answer == question.answer:
             score += 1
     print(f"{name} got {score} out of {str(len(questions))} questions correct")
 
 
+def play_again():
+    """
+    Function takes user input and determines
+    whether to play game again.
+    """
+    response = input("Do you want to play again? (y/n)")
+    response = response.lower()
+
+    if response == "y":
+        return True
+    else:
+        return False
+
+
 run_quiz(questions)
+
+while play_again():
+    run_quiz(questions)
+print(f"Goodbye {name}")
