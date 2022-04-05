cdf030a07c17a58b911123be64b3d5c73cce9102
AriRix1410 <aa.fraserhaste@gmail.com>
2022-04-05 15:00:02 +0000

    Add list of questions and import into run.py

---
+++ questions.py
@@ -0,0 +1,7 @@
+question_prompts = [
+    "When was A New Hope released?\n(a) 1976\n(b) 1977\n(c) 1978\n(d) 1979\n\n",
+    "Which actor is the only one who's appeared in every Star Wars movie to date??\n(a) Mark Hamill\n(b) Carrie Fisher\n(c) Harrison Ford\n(d) Anthony Daniels\n\n",
+    "In which film does Luke find out Darth Vader is his father?\n(a) A New Hope\n(b) Empire Strikes Back\n(c) Return of the Jedi\n(d) Attack of the Clones\n\n",
+    "How old was Yoda when he died?\n(a) 600\n(b) 700\n(c) 800\n(d) 900\n\n",
+    "What was the original name of the Star Wars series?\n(a) Star Battles\n(b) Space Adventures\n(c) Adventures of Luke Starkiller\n(d) Space Wars\n\n",
+]
